7d3fb9a550194afe22472bc605fd9a0178a52ef0
shanke <shanke@qunhemail.com>
2021-12-24 15:58:04 +0800

    init code

--- src/1.targetSum/index.ts
+++ src/1.targetSum/index.ts
@@ -1,7 +1,36 @@
-function sum(a: number, b: number) {
-    return a + b;
+/**
+给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。你可以按任意顺序返回答案。
+
+输入：nums = [2,7,11,15], target = 9
+输出：[0,1]
+解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 
+ */
+
+function getTwoSumIndex(nums: Array<number>, targetSum: number) {
+    debugger
+    let result = null;
+    if (!nums || nums.length < 2) {
+        return result;
+    }
+
+    const hashMap = {};
+    nums.forEach((item: number, index: number) => {
+        hashMap[index] = targetSum - item;
+    });
+
+    const length = nums.length;
+    for (let i = 0; i < length; i++) {
+        const j = nums.indexOf(hashMap[i]);
+        if (j > -1 && j !== i) {
+            result = [i, j];
+            break;
+        }
+    }
+
+    return result;
 }
 
 export {
-    sum
-};
+    getTwoSumIndex
+}
--- test/1.targetSum/index.ts
+++ test/1.targetSum/index.ts
@@ -1,8 +1,25 @@
-import { sum } from '../../src/1.targetSum/index';
+import { getTwoSumIndex } from '../../src/1.targetSum/index';
 import { expect } from 'chai';
 
-describe('sum', () => {
-    it ('expect 2 + 3 = 5', () => {
-        expect(sum(2, 3)).to.be.equal(5);
-    })
+describe('TargetSum: Get two number index:', () => {
+    it ('1.expect [1, 2, 3, 4, 5 , 6] two numbers sum is 6, result: [0, 4]', () => {
+        const arr = [1, 2, 3, 4, 5 , 6];
+        const result = getTwoSumIndex(arr, 6);
+        expect(result).to.deep.equal([0, 4]);
+    });
+
+    // it ('2.expect [1, 2, 3] two numbers sum is 6, result: null', () => {
+    //     const arr = [1, 2, 3];
+    //     expect(getTwoSumIndex(arr, 6)).to.deep.equal(null);
+    // });
+
+    // it ('3.expect [1, 2, 3, 4, 5 , 6, 7, 8] two numbers sum is 13, result: [4, 7]', () => {
+    //     const arr = [1, 2, 3, 4, 5 , 6, 7, 8];
+    //     expect(getTwoSumIndex(arr, 13)).to.deep.equal([4, 7]);
+    // });
+
+    // it ('4.expect [1, 2, 3, 4, 5 , 6, 7, 8] two numbers sum is 13, result not: [2, 5]', () => {
+    //     const arr = [1, 2, 3, 4, 5 , 6, 7, 8];
+    //     expect(getTwoSumIndex(arr, 13)).to.not.deep.equal([2, 5]);
+    // });
 });
